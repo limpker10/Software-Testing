5d33f9305c6ec78ad610d5f6c458f5c370f7ced3
José Miguel <jcanov@unsa.edu.pe>
2023-05-13 16:40:15 -0500

    test: agregando tests deposito y ver saldo

--- Lab01/test_atm.py
+++ Lab01/test_atm.py
@@ -6,20 +6,29 @@ class CajeroTest(unittest.TestCase):
       self.cajero = Cajero()
   
   def test_retiro_saldo_suficiente(self):
-      self.assertTrue(self.cajero.validar_retiro(23))
+      self.assertTrue(self.cajero.validar_retiro(5000))
 
   def test_retiro_saldo_insuficiente(self):
       self.assertFalse(self.cajero.validar_retiro(5001))
 
   def test_retiro_maximo(self):
-      
       self.assertTrue(self.cajero.retiroMaximo(2000))
       self.assertFalse(self.cajero.retiroMaximo(4000))
+  
+  def test_depositar(self):
+      self.assertEqual(self.cajero.realizar_deposito(2343), (5000+2343))
 
   def test_deposito_maximo(self):
-      
       self.assertTrue(self.cajero.depositoMaximo(5000))
       self.assertFalse(self.cajero.depositoMaximo(15000))
+
+  def test_ver_saldo(self):
+      self.assertEqual(self.cajero.obtener_saldo(),5000)
+      self.cajero.realizar_deposito(20);
+      self.assertEqual(self.cajero.obtener_saldo(),5020)
+      self.cajero.realizar_retiro(20);
+      self.assertEqual(self.cajero.obtener_saldo(),5000)
+
 
 if __name__ == '__main__':
     unittest.main()
