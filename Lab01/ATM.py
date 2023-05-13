682ed85c598141b02fe572b9759092cd15966a1d
José Miguel <jcanov@unsa.edu.pe>
2023-05-13 10:15:49 -0500

    refactor: seleccion de opciones menu, agregando variables RETIRO_MAX, DEPOSITO_MAX

--- Lab01/ATM.py
+++ Lab01/ATM.py
@@ -7,8 +7,11 @@ Created on Tue May  9 16:46:29 2023
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
-password = int(os.getenv("PASSWORD"))
+PASSWORD = int(os.getenv("PASSWORD"))
+RETIRO_MAX = 3000
+DEPOSITO_MAX = 10000
 
 
 class Cajero:
@@ -21,7 +24,7 @@ class Cajero:
         contador = 1
         while contador <= 3:
             x = int(input("ingrese su contraseña:"))
-            if x == password:
+            if x == PASSWORD:
                 print("Contraseña Correcta")
                 return self.continuar
             else:
@@ -30,68 +33,109 @@ class Cajero:
                     print("No puede realizar operaciones.")
                     self.continuar = False
                 contador += 1
-                
+
         return self.continuar
     
+
+
     def menu(self):
         os.system("cls")  # esto es solo para windows
-        if self.contraseña() :
+        if self.contraseña():
             opcion = 0
             while opcion != "4":
-                os.system("cls")
                 print(
                     """ Bienvenido al cajero automatico
                 ******Menú******
-                1-  Depositar
+                1- Depositar
                 2- Retirar
                 3- Ver saldo
                 4- Salir """
                 )
                 opcion = input("Su opción es: ")
+
+                actions = {
+                    "1": self.depositar,
+                    "2": self.retiro,
+                    "3": self.ver
+                }
+
                 if self.continuar:
-                    if opcion == "1":
-                        self.depositar()
-                    elif opcion == "2":
-                        self.retiro()
 
-                    elif opcion == "3":
-                        self.ver()
-
-                    elif opcion == "4":
-                        print("Programa finalizado")
-
-                    else:
-                        print("NO existe esa opción")
-
-                else:
-                    if opcion in "123":
+                    if opcion not in "123":
                         print("Imposible realizar esa operación")
+                        return
 
-                    elif opcion == "4":
+                    if opcion == "4":
                         print("Programa finalizado")
-
-                    else:
+                        return
+                    
+                    if opcion == "":
                         print("No existe esa opción")
+                        return
+                    
+                    action = actions.get(opcion)
+                    action()
 
+                
+
+                # if self.continuar:
+                #     if opcion == "1":
+                    #         self.depositar()
+                #     elif opcion == "2":
+                #         self.retiro()
+
+                #     elif opcion == "3":
+                #         self.ver()
+
+                #     elif opcion == "4":
+                #         print("Programa finalizado")
+
+                #     else:
+                #         print("NO existe esa opción")
+
+                # else:
+                #     if opcion in "123":
+                #         print("Imposible realizar esa operación")
+
+                #     elif opcion == "4":
+                #         print("Programa finalizado")
+
+                #     else:
+                #         print("No existe esa opción")
+
+   
     def depositar(self):
         dep = int(input("Ingrese su monto a depositar:"))
-        print("Usted a depositado", dep)
-        print(f"Su nuevo saldo es {self.monto + dep}")
-        self.monto += dep
+        if not self.depositoMaximo(dep):
+            print("El deposito excede el límite maximo permitido.")
+            return
 
+        print("Usted a depositado", dep)
+        print(f"Su nuevo saldo es {self.monto + dep}")  
+        self.monto += dep
+        
     def retiro(self):
         retirar = int(input("¿Cuánto desea retirar? : "))
+        if not self.retiroMaximo(retirar):
+            print("El retiro excede el límite maximo permitido.")
+            return
+        
         print("Su monto actual es", self.monto)
-        if self.monto >= girar:
+        if self.monto >= retirar:
             print(
                 f"Usted a retirado: {retirar} , su nuevo monto es {self.monto - retirar}"
             )
             self.monto -= retirar
         else:
             print("Imposible realizar el retiro, su monto es menor")
-
+            
     def ver(self):
         print("Su saldo es: ", self.monto)
 
+    def retiroMaximo(retirar):
+        return  retirar <= RETIRO_MAX;
+    
+    def depositoMaximo(self,deposito):
+        return  deposito <= DEPOSITO_MAX;
 
 app = Cajero()
