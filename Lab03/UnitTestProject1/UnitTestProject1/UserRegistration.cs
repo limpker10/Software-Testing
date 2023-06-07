10c77d6e0927192adbcefbf6d979dfddc899228f
Jose Cano <jcanov@unsa.edu.pe>
2023-06-07 17:23:24 -0500

    Terminando Lab03

---
+++ Lab03/UnitTestProject1/UnitTestProject1/UserRegistration.cs
@@ -0,0 +1,17 @@
+﻿public class UserRegistration
+{
+    public bool Register(string username, string email, string password)
+    {
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            return false;
+        }
+
+        if (password.Length < 8)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Lab03/UnitTestProject1/UnitTestProject1/UserRegistrationTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UserRegistrationTests
+    {
+        private UserRegistration registration;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+            registration = new UserRegistration();
+        }
+
+        public static IEnumerable<object[]> TestCases()
+        {
+            // Caso positivo
+            yield return new object[] { "john", "john@example.com", "password", true };
+            
+            // Caso username vacío
+            yield return new object[] { "", "john@example.com", "password", false };
+
+            // Caso límite inferior - contraseña demasiado corta      
+            yield return new object[] { "user", "user@example.com", "pass", false };
+
+            // Caso null - username nulo
+            yield return new object[] { null, "john@example.com", "password", false };
+
+            // Caso null - email nulo
+            yield return new object[] { "john", null, "password", false };
+
+            // Caso null - contraseña nula
+            yield return new object[] { "john", "john@example.com", null, false };
+
+            // Caso límite superior - contraseña con 32 caracteres
+            yield return new object[] { "john", "john@example.com", "passwordpasswordpasswordpassword", true };
+
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(TestCases), DynamicDataSourceType.Method)]
+        public void Register_TestCases(string username, string email, string password, bool expectedResult)
+        {
+            
+            bool actualResult = registration.Register(username, email, password);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
